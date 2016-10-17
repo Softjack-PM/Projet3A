831943c430ba9b4c8c07540dc5a13df3de3e399d
Softjack-PM <pm.nicolezeau@orange.fr>
2016-10-17 14:27:51 +0200

    debut Toolbar

--- app/src/main/java/com/example/pierre_marie/projet3a/MainActivity.java
+++ app/src/main/java/com/example/pierre_marie/projet3a/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.pierre_marie.projet3a;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+// User chose the "Settings" item, show the app settings UI...
+                return true;
+            case R.id.action_load:
+// User chose the refresh action, refresh the page
+                return true;
+            default: // If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
