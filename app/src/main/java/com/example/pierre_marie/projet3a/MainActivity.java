72b506f02ef54493a9d83d7c9f9e627ee5ae57bc
Baygod <leomazard@gmail.com>
2016-10-25 16:05:04 +0200

    tes

--- app/src/main/java/com/example/pierre_marie/projet3a/MainActivity.java
+++ app/src/main/java/com/example/pierre_marie/projet3a/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pierre_marie.projet3a;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,7 +13,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback  {
 
     private GoogleMap mMap;
 
@@ -18,12 +21,20 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
     @Override
@@ -35,10 +46,24 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         //moovecamera
     }
 
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+// User chose the "Settings" item, show the app settings UI...
+                return true;
+            case R.id.action_load:
+// User chose the refresh action, refresh the page
+                return true;
+            default: // If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
+
+
 
 
 }
