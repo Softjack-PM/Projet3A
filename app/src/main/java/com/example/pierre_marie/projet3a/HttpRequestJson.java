c909a1c67a85302f871f612bd3e6965124280024
Softjack-PM <pm.nicolezeau@orange.fr>
2016-10-30 23:58:58 +0100

    Requête Http et listeview

---
+++ app/src/main/java/com/example/pierre_marie/projet3a/HttpRequestJson.java
@@ -0,0 +1,84 @@
+package com.example.pierre_marie.projet3a;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Pierre-Marie on 30/10/2016.
+ */
+
+public class HttpRequestJson {
+
+    private List<Monument> monumentInfo;
+
+    public HttpRequestJson() {
+        this.monumentInfo = new ArrayList<>();
+    }
+
+    public void LaunchHttpRequestJson (final RequestQueue requestQueue, final MainActivity CurrentActivity, String URL_API) {
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL_API, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+
+                try {
+                    Gson gson = new GsonBuilder().create();
+
+
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject informations = response.getJSONObject(i);
+//                        String proprietes = informations.getString("");
+                        String nom = informations.getString("name");
+                        String latitude = informations.getString("latitude");
+                        String longitude = informations.getString("longitude");
+
+                        Monument monumentInfo = new Monument();
+                        monumentInfo.setName(nom);
+                        monumentInfo.setLatitude(Double.parseDouble(latitude));
+                        monumentInfo.setLongitude(Double.parseDouble(longitude));
+//                        Monument monumentInfo = gson.fromJson(proprietes,Monument.class);
+
+                        getMonumentList().add(monumentInfo);
+                    }
+                    Log.d("Monument : ", monumentInfo.toString());
+
+                    Toast.makeText(CurrentActivity, "Données recupérées", Toast.LENGTH_SHORT).show();
+                    CurrentActivity.httpRequestReceived(true);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            }, new Response.ErrorListener() { // Erreur dans la requete
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    CurrentActivity.httpRequestReceived(false);
+                    Toast.makeText(CurrentActivity, "Impossible de récupérer les données", Toast.LENGTH_SHORT).show();
+                }
+            });
+        requestQueue.add(jsonArrayRequest);
+
+    }
+
+    public List<Monument> getMonumentList() {
+        return monumentInfo;
+    }
+}
---
+++ app/src/main/java/com/example/pierre_marie/projet3a/Monument.java
@@ -0,0 +1,48 @@
+package com.example.pierre_marie.projet3a;
+
+/**
+ * Created by Pierre-Marie on 30/10/2016.
+ */
+
+import com.google.gson.annotations.SerializedName;
+
+public class Monument {
+
+    @SerializedName("name")
+    private String name;
+    @SerializedName("latitude")
+    private double latitude;
+    @SerializedName("longitude")
+    private double longitude;
+
+    public Monument(){
+    }
+
+    public String getName(){
+        return name;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Monument{" + "nom = " + name + "\nlatitude = " + latitude + "\nlongitude = " + longitude + "}";
+    }
+}
